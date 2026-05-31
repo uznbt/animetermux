c0bbf6a2c1f5c5a21817b044d38dec989ba12b63
uznbt <uznbt25@gmail.com>
2026-06-01 03:54:51 +0700

    Fix scroll position overwrite bug during page transitions

---
+++ frontend/dist/assets/chevron-left-clean-CldAiBwC.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-clean-BZPhJY2i.js";var a=e("chevron-left",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);export{a as t};
--- frontend/dist/assets/chevron-left-clean-kmbsfZwz.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-clean-BWooRet_.js";var a=e("chevron-left",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);export{a as t};
